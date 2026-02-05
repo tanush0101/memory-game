57ffbc333a7212ccdec9aa95c8d02f1aa82239d2
Tanush <tanushh.ailani@gmail.com>
2026-02-05 18:43:40 +0530

    initial commit

---
+++ src/app/app-module.ts
@@ -0,0 +1,19 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import { App } from './app';
+
+@NgModule({
+  declarations: [
+    App
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideClientHydration(withEventReplay()),
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
